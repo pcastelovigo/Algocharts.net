271f23c57007b496453e5506821d4f740e9890ff
guionazo <pablocv@gmx.es>
2021-10-23 20:11:56 +0200

    correct query & working selector

--- charts/index-30.php
+++ charts/index-30.php
@@ -1,10 +1,11 @@
 <?php
 $servername = "localhost";
-$username = "test";
+$username = "pablo";
 $password = "test1";
 $asset_in = $_GET['asset_in'];
 $asset_out = $_GET['asset_out'];
 if(!$_GET){ $asset_in = "0"; $asset_out = "330109984"; }
+
 $dbname = "nombres";
 
 $conn = new mysqli($servername, $username, $password, $dbname);
@@ -45,39 +46,79 @@ width: 1360px;
 margin: auto;
 }
 </style>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.5.0/Chart.min.js"></script>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
 </head>
 
 <body>
 <div class="w3-container w3-row principal">
-<p>Hello tinies!! I made a <a href="https://github.com/pcastelovigo/freetinycharts">small, easy to setup software</a> that automagically grabs all new&existing Tinyman pools, stores price data and create charts for them.</p>
-<p>I'm opensourcing it so everybody can start their own Tinyman charts service! Also all historical ASA price data is avaliable for download for free <a href="precios.tar.bzip2">here</a>.</p>
+<p>Hello tinies!! I made a small, easy to setup software (this page!!) that automagically grabs all new&amp;existing Tinyman pools, stores price data and create charts for them.</p>
+<p>I'm opensourcing it so everybody can start their own Tinyman charts service! Also all historical ASA price data is avaliable for download for free <a href="precios.sql.bz2">here</a>.</p>
 <br>
 <p>If you want to help me to develop this and other Tinyman software and maintain this server, you can:</p>
-<p>- Contributing to my github</p>
+<p>- <a href="https://github.com/pcastelovigo/freetinycharts">Contributing to my github</a></p>
 <p>- Donating $algo to ARSXJS26M6M3MZUXJUYIWA4HOF5XKDEDDBYHEJS3OCCGUVCKQN3JNHXGOQ .Any amount will be appreciated.</p>
-<p>- Buying my for-fun <a href="https://app.tinyman.org/#/swap?asset_in=0&asset_out=330109984">ASA ID 330109984</a></p>
+<p>- Buying my <a href="https://app.tinyman.org/#/swap?asset_in=0&amp;asset_out=330109984">ASA ID 330109984</a></p>
 <p>Thank you very much!</p>
+<br>
 
+
+<div class="principal">
+<?php
+$dbname = "nombres"; $conn = new mysqli($servername, $username, $password, $dbname); 
+if ($conn->connect_error) {die("Connection failed: " . $conn->connect_error);}
+$sql = "select asset_id, nombre from nombres";
+$result = $conn->query($sql);
+?>
+        <select id="ASSET-IN" onchange="change_asset();">
+        <option value="Select asset IN">ASSET IN</option>
+<?php
+    while ($row = mysqli_fetch_array($result)) {
+        echo "<option value=" . $row['asset_id'] . ">" . $row['asset_id'] ." - ". $row['nombre']. "</option>"; }
+?>
+        </select>
+
+
+<select id="ASSET-OUT">
+<option value="Select asset OUT">ASSET OUT</option>
+
+
+</select>
+<input type="button" value="Go to chart" onclick = "Goto()" />
+</div>
 </div>
 
 <div class="w3-container w3-row principal">
 <div class="w3-twothird w3-container">
-<h2><?php echo "".$nombre1." TO ".$nombre2." 48h view"; ?></h2>
+<h2><?php echo "".$nombre1." TO ".$nombre2." 30 day view"; ?></h2>
 <canvas id="grafica" width="1000" style="margin: auto; max-width: 100%"></canvas>
 </div>
 <div class="w3-container w3-third">
 <br>
-<a href="index.php">48 hours chart</a>
+<h2>Other charts</h2>
+<a href=<?php echo '"'."https://freetinycharts.ovh/index.php?asset_in=".$asset_in."&amp;asset_out=".$asset_out.'"' ?>>48 hours chart</a>
 <br>
-<a href="index-30.php">1 month chart</a>
+<a href=<?php echo '"'."https://freetinycharts.ovh/index-30.php?asset_in=".$asset_in."&amp;asset_out=".$asset_out.'"' ?>>1 month chart</a>
 <br>
-<a href="index-1y.php">1 year chart</a>
+<a href=<?php echo '"'."https://freetinycharts.ovh/index-1y.php?asset_in=".$asset_in."&amp;asset_out=".$asset_out.'"' ?>>1 year chart</a>
+<h2>Popular pools</h2>
+<a href="https://freetinycharts.ovh/index-30.php?asset_in=226701642&amp;asset_out=0">Yieldy/Algorand</a>
+<br>
+<a href="https://freetinycharts.ovh/index-30.php?asset_in=27165954&amp;asset_out=0">Planet/Algorand</a>
+<br>
+<a href="https://freetinycharts.ovh/index-30.php?asset_in=230946361&amp;asset_out=31566704">AlgoGems/USDC</a>
+<br>
+<a href="https://freetinycharts.ovh/index-30.php?asset_in=230946361&amp;asset_out=0">AlgoGems/Algorand</a>
+<br>
+<a href="https://freetinycharts.ovh/index-30.php?asset_in=137594422&amp;asset_out=0">HEADLINE/Algorand</a>
+<br>
 </div>
+
 
 <div class="w3-container w3-twothird">
 <div class="icons" style="margin-left: 42px">
 <figure>
-<a href=<?php echo '"'."https://freetinycharts.ovh/index.php?asset_in=".$asset_out."&amp;asset_out=".$asset_in.'"' ?>>
+<a href=<?php echo '"'."https://freetinycharts.ovh/index-30.php?asset_in=".$asset_out."&amp;asset_out=".$asset_in.'"' ?>>
 <img class="icons" src="shuffle.webp" width="64" title="<?php echo 'Exchange rate for '.$nombre2.' TO '.$nombre1 ?>"></a>
 <figcaption>Switch assets</figcaption>
 </figure>
@@ -93,18 +134,17 @@ margin: auto;
 </div>
 
 <div class="w3-container w3-third">
-X
+@
 </div>
 </div>
 
-<script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.5.0/Chart.min.js"></script>
 <script>
 const labels = [
 <?php
 $dbname = "precios_diario";
 $conn = new mysqli($servername, $username, $password, $dbname);
 if ($conn->connect_error) {die("Connection failed: " . $conn->connect_error);}
-$sql = "SELECT fecha FROM ".$asset_in."_".$asset_out." WHERE id % 14 = 0 ORDER BY id ASC LIMIT 196";
+$sql = "SELECT fecha FROM (SELECT * FROM ".$asset_in."_".$asset_out." WHERE id % 14 = 0 ORDER BY id DESC LIMIT 196) t2 ORDER BY t2.id ASC";
 $result = $conn->query($sql);
 if ($result->num_rows > 0) { while($row = $result->fetch_assoc()) { echo "'". $row["fecha"]. "', "; } } else { echo "Error! You choosed non existing pairs."; }
 $conn->close(); ?>
@@ -120,7 +160,7 @@ const data = {
 $dbname = "precios_diario";
 $conn = new mysqli($servername, $username, $password, $dbname);
 if ($conn->connect_error) { die("Connection failed: " . $conn->connect_error); }
-$sql = "SELECT precio FROM ".$asset_in."_".$asset_out." WHERE id % 14 = 0 ORDER BY id ASC LIMIT 196";
+$sql = "SELECT precio FROM (SELECT * FROM ".$asset_in."_".$asset_out." WHERE id % 14 = 0 ORDER BY id DESC LIMIT 196) t2 ORDER BY t2.id ASC";
 $result = $conn->query($sql);
 if ($result->num_rows > 0) { while($row = $result->fetch_assoc()) { echo $row["precio"]. ", "; } } else { echo "Error! You choosed non existing pairs."; }
 $conn->close(); ?>
@@ -141,45 +181,36 @@ const config = {
 </script>
 
 <script>
- // === include 'setup' then 'config' above ===
-
   var myChart = new Chart(
     document.getElementById('grafica'),
     config
   );
 </script>
-<div class="principal">
-Be sure of choosing existing Tinyman pairs.
-<?php
-$dbname = "nombres";
-$conn = new mysqli($servername, $username, $password, $dbname);
-if ($conn->connect_error) {die("Connection failed: " . $conn->connect_error);}
-$sql = "select asset_id, nombre from nombres";
-$result = $conn->query($sql);
-?>
-	<select id="ASSET-IN">
-	<option value="Select asset IN">ASSET IN</option>
-<?php
-    while ($row = mysqli_fetch_array($result)) {
-        echo "<option value=" . $row['asset_id'] . ">" . $row['asset_id'] ." - ". $row['nombre']. "</option>"; }
-?>
-	</select>
-	<select id="ASSET-OUT">
-	<option value="Select asset OUT">ASSET OUT</option>
-<?php
-	mysqli_data_seek($result, 0);
-	while ($row = mysqli_fetch_array($result)) {
-        echo "<option value=" . $row['asset_id'] . ">" . $row['asset_id'] ." - ". $row['nombre']. "</option>"; }
-?>
-	</select>
 
-<input type="button" value="Go to chart" onclick = "Goto()" />
-</div>
-<script type="text/javascript">
+
+<script>
+function change_asset()
+{
+	var pares = $("#ASSET-IN").val();
+	   $.ajax({
+		type: "POST",
+		url: "pares.php",
+		data: "pares="+pares,
+		cache: false,
+		success: function(response)
+			{
+					//alert(response);return false;
+				$("#ASSET-OUT").html(response);
+			}
+			});
+}
+</script>
+
+<script>
 function Goto() {
   var x = document.getElementById("ASSET-IN").value;
   var y = document.getElementById("ASSET-OUT").value;
-  var url = "https://freetinycharts.ovh/index.php?asset_in=" + encodeURIComponent(x) + "&asset_out=" + encodeURIComponent(y);
+  var url = "https://freetinycharts.ovh/index-30.php?asset_in=" + encodeURIComponent(x) + "&asset_out=" + encodeURIComponent(y);
   window.location.href = url;
 };
 </script>
---
+++ charts/pares.php
@@ -0,0 +1,18 @@
+<?php
+$host= "localhost";
+$username= "pablo";
+$password = "test1";
+$db_name = "pares";
+$db_name2 = "nombres;";
+$db = new mysqli($host,$username,$password,$db_name);
+
+if($db->connect_error) { die("connection failed:". $db->connect_error); }
+
+$par = $_POST['pares'];
+$sql= "select assetout, nombre from pares where assetin='$par'";
+$query = $db->query($sql);
+echo '<option value="ASSET-OUT">ASSET OUT</option>';
+while($res = $query->fetch_assoc()){
+echo '<option value="'.$res['assetout'].'">'.$res['assetout']." - ".$res['nombre'].'</option>';
+}
+?>
